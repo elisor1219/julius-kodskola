62481ceaef6d737d091e4861079363435e5d1524
Benjamin Fransson <benjamin@cafcon.se>
2024-07-11 16:53:45 +0200

    addition to lession 1

--- 1-variables-and-constants/main.js
+++ 1-variables-and-constants/main.js
@@ -2,6 +2,10 @@
  * Your task is to create two variables. One named `name` and the other named `age`.
  * `name` should be a string and contain your name, with the first letter capitalized.
  * `age` should be a number and contain your age.
+ * 
+ * Hint: You can define variables using the `var`, `let` and `const` keywords.
+ *       Though you should avoid using `var`, we'll get into that later
+ * Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
  */
 
 /* DO NOT EDIT CODE ABOVE THIS LINE */
